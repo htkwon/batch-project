44378fe51d6b8f810d405073e3a2b98e90b38094
htkwon <a3001sd@gmail.com>
2023-07-12 15:50:28 +0900

    기능 추가
    - Batch 이용 기간에 따른 만료 코드 추가
    -

---
+++ src/main/java/com/example/batchproject/job/pass/ExpirePassesJobConfig.java
@@ -0,0 +1,100 @@
+package com.example.batchproject.job.pass;
+
+
+import com.example.batchproject.entity.PassEntity;
+import com.example.batchproject.status.PassStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class ExpirePassesJobConfig {
+
+    private final int CHUNCK_SIZE = 5;
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+
+    @Bean
+    public Job expirePassJob(){
+        return this.jobBuilderFactory.get("expirePassJob")
+                .start(expirePassStep())
+                .build();
+    }
+    @Bean
+    public Step expirePassStep(){
+        return this.stepBuilderFactory.get("expirePassStep")
+                .<PassEntity,PassEntity>chunk(CHUNCK_SIZE)
+                .reader(expirePassesItemReader())
+                .processor(expirePassesItemProcessor())
+                .writer(expirePassesItemWriter())
+                .build();
+    }
+
+
+    @Bean
+    @StepScope
+    public JpaCursorItemReader<PassEntity> expirePassesItemReader(){
+        return new JpaCursorItemReaderBuilder<PassEntity>()
+                .name("expirepassesItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .queryString("select p from PassEntity p where p.status =:status and p.endedAt <= :endedAt")
+                .parameterValues(Map.of("status", PassStatus.PROGRESSED,"endedAt", LocalDateTime.now()))
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<PassEntity,PassEntity> expirePassesItemProcessor(){
+        return passEntity ->{
+            passEntity.setPassStatus(PassStatus.EXPIRED);
+            passEntity.setExpiredAt(LocalDateTime.now());
+            return passEntity;
+        };
+    }
+
+    @Bean
+    public JpaItemWriter<PassEntity> expirePassesItemWriter(){
+        return new JpaItemWriterBuilder<PassEntity>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
