e1372e5fcfc2c44e897bbbf58dd84c09ef4cb411
htkwon <a3001sd@gmail.com>
2023-07-25 14:38:10 +0900

    수업 종료 후 이용권 차감 batch 기능 추가

--- src/main/java/com/example/batchproject/job/pass/AddPassesJobConfig.java
+++ src/main/java/com/example/batchproject/job/pass/AddPassesJobConfig.java
@@ -5,13 +5,12 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class AddPassesjobConfig {
+public class AddPassesJobConfig {
 
 
     private final JobBuilderFactory jobBuilderFactory;
